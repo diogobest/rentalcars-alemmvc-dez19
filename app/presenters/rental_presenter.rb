bf412aa2f90fcedd0c237ffc289afc1d05ed4f9b
Diogo <diogo.oliveira44@gmail.com>
2019-12-03 19:08:30 -0300

    bla'

--- app/presenters/rental_presenter.rb
+++ app/presenters/rental_presenter.rb
@@ -1,43 +1,44 @@
 class RentalPresenter < SimpleDelegator
   attr_reader :rental
 
-    def initialize(rental)
-        # @rental = rental
-        super(rental) # chamando o initializer do simpledelegator
-    end
+  def initialize(rental)
+    # @rental = rental
+    super(rental) # chamando o initializer do simpledelegator
+  end
 
-    def status
-      # if rental.scheduled?
-      if scheduled?
-        helper.content_tag :span, class: 'badge badge-primary' do
-          'agendada'
-        end
-      elsif finalized?
-        helper.content_tag :span, class: 'badge badge-success' do
-          'finalizada'
-        end
-      elsif in_review?
-        helper.content_tag :span, class: 'badge badge-warning' do
-          'agendada'
-        end
-      else ongoing?
-        helper.content_tag :span, class: 'badge badge-info' do
-          'agendada'
-        end
+  def status
+    # if rental.scheduled?
+    if scheduled?
+      helper.content_tag :span, class: 'badge badge-primary' do
+        'agendada'
+      end
+    elsif finalized?
+      helper.content_tag :span, class: 'badge badge-success' do
+        'finalizada'
+      end
+    elsif in_review?
+      helper.content_tag :span, class: 'badge badge-warning' do
+        'agendada'
+      end
+    else ongoing?
+      helper.content_tag :span, class: 'badge badge-info' do
+        'agendada'
       end
     end
-    private
+  end
 
-    def helper
-      ApplicationController.helpers # Carregando todos os helpers dentro desse método.
-    end
-    # private
+  private
 
-    # def method_missing(method, *args, &block)
-    #   rental.public_send(method, *args, &block)
-    # end
+  def helper
+    ApplicationController.helpers # Carregando todos os helpers dentro desse método.
+  end
+  # private
 
-    # def respond_to(method_name, include_private = false)
-    #   rental.respond_to? method_name
-    # end
-end 
+  # def method_missing(method, *args, &block)
+  #   rental.public_send(method, *args, &block)
+  # end
+
+  # def respond_to(method_name, include_private = false)
+  #   rental.respond_to? method_name
+  # end
+end
